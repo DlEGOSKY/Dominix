ba6aa2e6b070d2ea6e74970d530a2c963a5c0704
DlEGOSKY <134379604+DlEGOSKY@users.noreply.github.com>
2026-04-06 06:40:51 -0600

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
+declare const process: any;
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+const repoName = "Dominix";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/Dominix/",
+  base: isGitHubPages ? `/${repoName}/` : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
